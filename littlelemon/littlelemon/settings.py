8192ccc91835b8e96ddc593bb895fee7a79ca39d
KIA <sy.uname@gmail.com>
2023-02-22 12:27:12 +0400

    Securing the table booking API

--- littlelemon/littlelemon/settings.py
+++ littlelemon/littlelemon/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'djoser',
     'restaurant',
 ]
 
@@ -137,3 +138,14 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ],
+}
+
+
+DJOSER={"USER_ID_FIELD":"username"}
